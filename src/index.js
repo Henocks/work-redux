65d12889e37e14592bdd98ef00253d1ed4a0c1cd
Henocks <hr4574@naver.com>
2018-08-02 15:42:49 +0900

    Basic Source

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
 registerServiceWorker();
