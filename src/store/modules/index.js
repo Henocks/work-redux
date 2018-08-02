3980a1b602f9198697ae32a2dafdf23c92ea6bad
Henocks <hr4574@naver.com>
2018-08-02 15:22:05 +0900

    change and cleaning for new work

--- src/store/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-import configure from './configure';
-
-export default configure;
-export default combineReducers({
-    counter
-});
